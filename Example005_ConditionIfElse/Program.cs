8cd920fef44d78971b0c6154403bedcc4daad176
Семён <karachinstar@yandex.ru>
2023-01-14 18:25:11 +0800

    Example 005 If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+
+{
+    Console.WriteLine("Ура, это же ДАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
